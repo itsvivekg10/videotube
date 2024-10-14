484e0c924c6eb47561ffc039ee0e7f3656d1655a
itsvivekg10 <itsvivekg10@gmail.com>
2024-10-14 21:02:57 +0530

    created search functionality in header component

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,36 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import "./Feed.css";
 import thumbnail1 from "../../assets/thumbnail1.png";
 import { API_KEY } from "../../data/data";
 import { Link } from "react-router-dom";
 import moment from "moment/moment";
 import { value_converter } from "../../data/data";
-const Feed = ({ category, data, setData }) => {
-  // State to store fetched data
-  // const [data, setData] = useState([]);
 
-  // Fetch data from YouTube API
+const Feed = ({ category, data, setData, setSearchQuery, searchData }) => {
   const fetchData = async () => {
     const videoListUrl = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=us&videoCategoryId=${category}&key=${API_KEY}`;
 
     try {
       const response = await fetch(videoListUrl);
       const result = await response.json();
-      setData(result.items); // Update state with fetched data
+      setData(result.items);
       console.log(result);
     } catch (error) {
       console.error("Error fetching video data:", error);
     }
   };
 
-  // Call fetchData when the component mounts or when the category changes
   useEffect(() => {
     fetchData();
   }, [category]);
 
   return (
     <div className="feed">
-      {data.map((item, index) => {
+      {(searchData && searchData.length > 0 ? searchData : data).map((item) => {
         const { id, snippet } = item;
         const { title, channelTitle, thumbnails } = snippet;
 
@@ -38,12 +34,9 @@ const Feed = ({ category, data, setData }) => {
           <Link
             to={`video/${snippet.categoryId}/${id}`}
             className="video_sec"
-            key={id} // Add a unique key prop
+            key={id}
           >
-            <img
-              src={thumbnails?.medium?.url || thumbnail1} // Use API thumbnail or fallback
-              alt={title}
-            />
+            <img src={thumbnails?.medium?.url || thumbnail1} alt={title} />
             <div className="video-info">
               <h2>{title}</h2>
               <h3>{channelTitle}</h3>
