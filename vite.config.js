a4e225d91273b539711bbb07e3de2ab408181028
itsvivekg10 <itsvivekg10@gmail.com>
2024-10-14 23:48:04 +0530

    Simplified vite config

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/jokes": "http://localhost:",
-    },
-  },
-
   plugins: [react()],
 });
